93d45ba740591f99c14a389051f84597ed9a30ad
BuildTools <shyang2008@naver.com>
2021-05-27 10:21:07 +0900

    Kommand But JDK 1.8 and 1.12.2

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    kotlin("jvm") version "1.5.0"
+    `maven-publish`
+}
+
+repositories {
+    mavenCentral()
+    maven(url = "https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly(kotlin("stdlib"))
+    compileOnly("com.destroystokyo.paper:paper-api:1.12.2-R0.1-SNAPSHOT")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("org.mockito:mockito-core:3.6.28")
+    testImplementation("com.destroystokyo.paper:paper-api:1.13.2-R0.1-SNAPSHOT")
+}
+
+tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    create<Jar>("sourcesJar") {
+        from(sourceSets["main"].allSource)
+        archiveClassifier.set("sources")
+    }
+    test {
+        useJUnitPlatform()
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("Kommand") {
+            artifactId = project.name
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+        }
+    }
+}
